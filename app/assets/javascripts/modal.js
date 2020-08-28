0f89205f77281a93b5bbc49c02925fea10451191
ishii kentarou <ishikenken141@icloud.com>
2020-08-28 03:20:01 +0000

    ssssss

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,10 +1,22 @@
-$(function(){
-    $('#Modalopen').on('click',function(){
-        $('#exampleModal').fadeIn();
-        return false;
-    });
-    $('#btn').on('click',function(){
-        $('#exampleModal').fadeOut();
-        return false;
-    });
+document.addEventListener('DOMContentLoaded', function() { 　
+  var Modalopen = document.getElementById('#Modalopen')　
+  Modalopen.addEventListener('click', function() { 　
+  ("#exampleMotal").fadeIn(); 
+  return false;
+  });
 });
+
+
+
+
+
+// $(function(){
+//     $('#Modalopen').on('click',function(){
+//         $('#exampleModal').fadeIn();
+//         return false;
+//     });
+//     $('#btn').on('click',function(){
+//         $('#exampleModal').fadeOut();
+//         return false;
+//     });
+// });
